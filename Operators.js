433d975e3ea1b3deb9a55aced11ff1642dfa607c
srivyshnavi1505 <srivyshnavip1505@gmail.com>
2026-01-20 17:09:45 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,21 @@
+//Initial data:
+
+        let totalAmount = 0;
+
+
+ // 1. Add ₹500 to the total
+
+ totalAmount +=500
+
+  //2. Add ₹1200 to the total
+
+totalAmount += 1200
+
+ // 3. Apply a ₹200 discount
+ totalAmount -=200
+
+  //4. Add 18% GST
+totalAmount += totalAmount*0.18
+
+ // 5. Print the final bill amount
+ console.log(totalAmount)
